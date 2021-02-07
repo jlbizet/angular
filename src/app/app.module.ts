f7a07df20aeeaeaa2fcb38dad9a0668599511c45
Jose Luis <jlbizet90@gmail.com>
2021-02-07 15:41:59 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+
+import { AppComponent }      from './app.component';
+import { ContadorModule }    from './contador/contador.module';
+import { ContadorComponent } from './contador/contador/contador.component';
+import { DbzModule }         from './dbz/dbz.module';
+import { HeroeModule }       from './heroes/heroe.module';
+import { HeroeComponent }    from './heroes/heroe/heroe.component';
+import { ListadoComponent }  from './heroes/listado/listado.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+            imports: [
+              BrowserModule,
+              HeroeModule,
+              ContadorModule,
+              DbzModule
+            ],
   providers: [],
   bootstrap: [AppComponent]
 })
